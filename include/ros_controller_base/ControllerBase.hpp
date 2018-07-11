d74ffb52fd993a3b72dd298457bea1e591c6bc96
sadetra <m.efetiryaki@gmail.com>
2018-07-11 19:09:50 +0800

    ros_node_utils name chane is adopted

--- include/ros_controller_base/ControllerBase.hpp
+++ include/ros_controller_base/ControllerBase.hpp
@@ -73,7 +73,6 @@ class ControllerBase
       std::cerr<<"controller is running on real robot"<<std::endl;
     }
     nodeHandle_->getParam(ns_ + "/controller/rate", controllerRate_);
-    std::cerr<<"controller rate : "<<controllerRate_ << std::endl;
     dt_ = 1.0 / controllerRate_;
   }
   ;
